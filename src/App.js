88d4e2d71b397c93e186f8bdd77de2d30b8bcce2
Wesley Marques Pizetta <wesleywesley.marques@wizecompany.com.br>
2020-01-22 09:44:06 -0300

    using useState hook [class 44]

--- src/App.js
+++ src/App.js
@@ -2,45 +2,47 @@ import React, { useState } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
+//notes:
+//useState: the most important react hook; it allows us to manage state in a functional component
+//return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m a React App!!!')) is the compiled version of jsx format (mostly used)
+//method that you're not calling, but assigning as an event handler -> put the "Handler" word in these cases
+
 const app = props => {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a react App</h1>
-        <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-          <Person 
-            name={this.state.person[0].name} 
-            age={this.state.person[0].age} 
-          />
-          <Person 
-            name={this.state.person[1].name} 
-            age={this.state.person[1].age}
-          />
-          <Person 
-            name={this.state.person[2].name} 
-            age={this.state.person[2].age}
-          />
-      </div>
-    );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m a React App!!!')) compiler version of jsx above this
+  const [personState, setPerson] = useState({
+    person: [
+      { name: 'Wesley', age: 19 },
+      { name: 'Boglárka', age: 18 },
+      { name: 'Key', age: 20 }
+    ]
+  }); //it always return two elements; the first one is the own useState, the second one is a function that allow us to update the state element
+  const switchNameHandler = () => {
+    setPerson({
+      person: [
+        { name: 'Wesley Pizetta', age: 19 },
+        { name: 'Boglárka Bokor', age: 18 },
+        { name: 'Key', age: 20 }
+      ]
+    })
+  }  
+  return (
+    <div className="App">
+      <h1>Hi, I'm a react App</h1>
+      <p>This is really working!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+        <Person 
+          name={personState.person[0].name} 
+          age={personState.person[0].age} 
+        />
+        <Person 
+          name={personState.person[1].name} 
+          age={personState.person[1].age}
+        />
+        <Person 
+          name={personState.person[2].name} 
+          age={personState.person[2].age}
+        />
+    </div>
+  );
 }
 
 export default app;
-
-const stateArr = useState({
-  person: [
-    { name: 'Wesley', age: 19 },
-    { name: 'Boglárka', age: 18 },
-    { name: 'Key', age: 20 }
-  ]
-});
-
-switchNameHandler = () => {
-  this.setState({
-    person: [
-      { name: 'Wesley Pizetta', age: 19 },
-      { name: 'Boglárka Bokor', age: 18 },
-      { name: 'Key', age: 20 }
-    ]
-  })
-} //method that you're not calling, but assigning as an event handler
