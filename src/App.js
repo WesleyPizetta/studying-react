9d7c2fb81ab1961b7a130188517b775a5d3d18db
Wesley Marques Pizetta <wesleywesley.marques@wizecompany.com.br>
2020-01-22 10:01:25 -0300

    using class components [class 43]

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
-const app = props => {
+  class App extends Component {
+    state = {
+      person: [
+        { name: 'Wesley', age: 19 },
+        { name: 'Boglárka', age: 18 },
+        { name: 'Key', age: 20 }
+      ]
+    };
+
+    switchNameHandler = () => {
+      this.setState({
+        person: [
+          { name: 'Wesley Pizetta', age: 19 },
+          { name: 'Boglárka Bokor', age: 18 },
+          { name: 'Key', age: 20 }
+        ]
+      })
+    } //method that you're not calling, but assigning as an event handler
+    render(){
     return (
       <div className="App">
         <h1>Hi, I'm a react App</h1>
@@ -21,26 +39,10 @@ const app = props => {
             age={this.state.person[2].age}
           />
       </div>
-    );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m a React App!!!')) compiler version of jsx above this
+    );// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'I\'m a React App!!!')) compiler version of jsx above this
+  }
 }
 
-export default app;
+export default App;
 
-const stateArr = useState({
-  person: [
-    { name: 'Wesley', age: 19 },
-    { name: 'Boglárka', age: 18 },
-    { name: 'Key', age: 20 }
-  ]
-});
 
-switchNameHandler = () => {
-  this.setState({
-    person: [
-      { name: 'Wesley Pizetta', age: 19 },
-      { name: 'Boglárka Bokor', age: 18 },
-      { name: 'Key', age: 20 }
-    ]
-  })
-} //method that you're not calling, but assigning as an event handler
